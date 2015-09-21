417d4a8c6ddeeb78559817531490989d33567ee9
lievenvg <lievenvg@gmail.com>
2015-09-21 12:14:43 +0200

    Page structure and sidenav setup

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -25,16 +25,39 @@
  .config(function ($routeProvider) { /*, $locationProvider */
   $routeProvider
   .when('/', {
-    templateUrl: 'views/main.html',
-    controller: 'MainCtrl'
+    templateUrl: 'views/alle-aanvragen.html',
+    //controller: 'MainCtrl'
+  })
+  .when('/alle-aanvragen', {
+    templateUrl: 'views/alle-aanvragen.html',
+  })
+  .when('/nog-te-behandelen', {
+    templateUrl: 'views/nog-te-behandelen.html',
+  })
+  .when('/alle-hotlists', {
+    templateUrl: 'views/alle-hotlists.html',
+  })
+  .when('/uitgevoerde-programmas', {
+    templateUrl: 'views/uitgevoerde-programmas.html',
+  })
+  .when('/parameters', {
+    templateUrl: 'views/parameters.html',
+  })
+  .when('/tarieven', {
+    templateUrl: 'views/tarieven.html',
+  })
+  .when('/standaardmails', {
+    templateUrl: 'views/standaardmails.html',
   })
   .otherwise({
     redirectTo: '/'
   });
-
   // $locationProvider.html5Mode({
   //   enabled: true,
   //   requireBase: false
   // });
   // $locationProvider.hashPrefix('!');
 });
+
+
+
