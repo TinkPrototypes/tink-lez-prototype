f4900fea8dce8a3778dd61d88a3eb90ec5621a8e
Lieven VG <lievenvg@gmail.com>
2015-09-22 15:59:23 +0200

    Working interactive table. Now for some cleaning.

--- src/scripts/controllers/tableCtrl.js
+++ src/scripts/controllers/tableCtrl.js
@@ -3,27 +3,58 @@
 angular.module('tinkApp')
   .controller('tableCtrl',['$scope','$timeout',function (scope,timeout) {
 
+    scope.data = {};
     // Override default items per page
     scope.pages = '5, 10, 25';
 
-    scope.changed = function(data,next){
-      console.log(data,scope.nums);
+    //!! scope.changed = function(data,next){
+    //   console.log(data,scope.nums);
 
-      timeout(function(){
-        // Data model
-        scope.data = [
-        ];
-        scope.totalitems = 200;
-        next();
-      },300);
+    //   timeout(function(){
+    //     // Data model
+    //     scope.data = [
+    //     ];
+    //     scope.totalitems = 200;
+    //     next();
+    //   },300);
+    // };
+    scope.changed = function(type,value,fn){
+      fn();
     };
 
-    scope.totalitems = 200;
-    scope.chan = function(prop,or,typ){
-      console.log('changed',prop,or,typ);
-      timeout(function(){
+    //!! scope.totalitems = 200;
+    // scope.chan = function(prop,or,typ){
+    //   console.log('changed',prop,or,typ);
+    //   timeout(function(){
+    //   // Data model
+    //   scope.data = [
+    //     {
+    //       firstname: 'Jasper',
+    //       lastname: 'Van Proeyen',
+    //       username: '@trianglejuice'
+    //     },
+    //     {
+    //       firstname: 'Tom',
+    //       lastname: 'Wuyts',
+    //       username: '@pxlpanic'
+    //     },
+    //     {
+    //       firstname: 'Kevin',
+    //       lastname: 'De Mulder',
+    //       username: '@clopin'
+    //     },
+    //     {
+    //       firstname: 'Vincent',
+    //       lastname: 'Bouillart',
+    //       username: '@BouillartV'
+    //     }
+    //   ];
+    // },300);
+    // };
+
+    //!! timeout(function(){
       // Data model
-      scope.data = [
+      scope.data.content = [
         {
           firstname: 'Jasper',
           lastname: 'Van Proeyen',
@@ -33,46 +64,9 @@ angular.module('tinkApp')
           firstname: 'Tom',
           lastname: 'Wuyts',
           username: '@pxlpanic'
-        },
-        {
-          firstname: 'Kevin',
-          lastname: 'De Mulder',
-          username: '@clopin'
-        },
-        {
-          firstname: 'Vincent',
-          lastname: 'Bouillart',
-          username: '@BouillartV'
         }
       ];
-    },300);
-    };
-
-    timeout(function(){
-      // Data model
-      scope.data = [
-        {
-          firstname: 'Jasper',
-          lastname: 'Van Proeyen',
-          username: '@trianglejuice'
-        },
-        {
-          firstname: 'Tom',
-          lastname: 'Wuyts',
-          username: '@pxlpanic'
-        },
-        {
-          firstname: 'Kevin',
-          lastname: 'De Mulder',
-          username: '@clopin'
-        },
-        {
-          firstname: 'Vincent',
-          lastname: 'Bouillart',
-          username: '@BouillartV'
-        }
-      ];
-    },700);
+    //!! },700);
 
     // The headers you want to show and whether they are already visible in the table
     scope.headers = [
@@ -101,14 +95,29 @@ angular.module('tinkApp')
     scope.perpageValue=[10,20,25,30,45];
 
     // Some actions that become available when you check one or multiple rows
-   scope.actions = [
+   //!! scope.actions = [
+   //    {
+   //      name: 'remove',
+   //      callback: function(items) {
+   //        angular.forEach(items, function(val) {console.log('ohoho',scope.data.indexOf(val));
+   //          scope.data.splice(scope.data.indexOf(val),1);
+   //        });
+   //      }
+   //    }
+   //  ];
+
+      scope.actions = [
       {
-        name: 'remove',
-        callback: function(items) {
-          angular.forEach(items, function(val) {console.log('ohoho',scope.data.indexOf(val));
-            scope.data.splice(scope.data.indexOf(val),1);
-          });
+          name: 'remove',
+          callback: function(items) {
+            angular.forEach(items, function(val) {
+              scope.data.content.splice(scope.data.content.indexOf(val),1);
+            });
+          },
+          order:0, //orde of the button
+          master:true, //required !
+          icon:'fa-close', //the icon required.
+          single:true // only when one checkbox is selected
         }
-      }
     ];
 }]);
